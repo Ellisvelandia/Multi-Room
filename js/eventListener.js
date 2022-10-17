979cda9f490703af202e9076d8c81152d6cf397a
ellisvelandia <eyis619@gmail.com>
2022-10-16 22:24:27 -0500

    player movement

---
+++ js/eventListener.js
@@ -0,0 +1,29 @@
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "w":
+      if (player.velocity.y === 0) player.velocity.y = -20;
+      break;
+
+    case "a":
+      // move player to the left
+      keys.a.pressed = true
+      break;
+    //  nove the player fot the right
+    case "d":
+      keys.d.pressed = true
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "a":
+      // move player to the left
+      keys.a.pressed = false
+      break;
+    //  nove the player fot the right
+    case "d":
+      keys.d.pressed = false
+      break;
+  }
+});
