80f904ab6feaf519a0d6cac38e43a9901af5a14b
ellisvelandia <eyis619@gmail.com>
2022-10-16 23:00:41 -0500

    collicion detect

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const c = canvas.getContext("2d");
 canvas.width = 64 * 16;
 canvas.height = 64 * 9;
 
+const parsedCollisions = collisionsLevel1.parse2D();
+const collisionBlocks = parsedCollisions.createObjectsFrom2D();
+
 const backgroundLevel1 = new Sprite({
   position: {
     x: 0,
@@ -32,6 +35,9 @@ function animate() {
   c.fillRect(0, 0, canvas.width, canvas.height);
 
   backgroundLevel1.draw();
+  collisionBlocks.forEach((collisionBlock) => {
+    collisionBlock.draw();
+  });
 
   player.velocity.x = 0;
   if (keys.d.pressed) player.velocity.x = 5;
